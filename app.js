c102eb3faa66106c0da0839ddf1e00e55234100c
fakegio <giodaza17@gmail.com>
2025-03-01 17:59:53 -0500

    fixed api key leak

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
@@ -7,12 +8,12 @@ const searchBtn = document.querySelector("#submitSearch");
 const searchInput = document.querySelector("#searchWord");
 const imageElement = document.querySelector("#imageContainer img");
 const feedback = document.querySelector("p");
-console.log(document.querySelector("p").innerText)
+const api_key = config.GIPHY_API_KEY;
 
 searchBtn.addEventListener("click", (e) => {
 
     let searchString = searchInput.value;
-    let translateURL = "https://api.giphy.com/v1/gifs/translate?api_key=U7oFIyV4PoXhPZowdA9GMh3CSOXAGggd&s="
+    let translateURL = "https://api.giphy.com/v1/gifs/translate?api_key=" + api_key + "&s="
 
     if (!searchString) {
         feedback.innerText = "Result cannot be empty";
@@ -24,7 +25,7 @@ searchBtn.addEventListener("click", (e) => {
         .then((response) => {
             imageElement.src = response.data.images.original.url;
             searchInput.value = "";
-            feedback.innerText = "";
+            feedback.innerText = searchString;
         })
         .catch((e) => {
             feedback.innerText = e.message;
